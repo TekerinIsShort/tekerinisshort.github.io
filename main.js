6e5a4b1d5ba748d21822fe13c557463623097a68
TekerinIsShort <tanklesscar123@yahoo.com>
2024-07-23 06:32:56 -0400

    testing to see mobile device support

--- main.js
+++ main.js
@@ -1,41 +1,178 @@
 
 
-console.log(document.getElementById('Test'));
 
-
-const elem = document.getElementById("A6");
+// get all forts within map
+const map = document.getElementById('map');
 const forts = document.getElementsByClassName("fort");
 
-// document.getElementById('generateScreenShot').addEventListener('click',function(){
+// global variable
+let isPugMode = true;
 
-// });
-
-console.log(elem);
-console.log(forts);
 
 function toggleFortColor(){
-
     const color = this.getAttribute("data-color");
-    if(color == "default"){
-        this.setAttribute("data-color","red");
-        this.src = "./red/" + this.id + ".png";
-    }
-    else if(color == "red"){
-        this.setAttribute("data-color","blue");
-        this.src = "./blue/" + this.id + ".png";
+    console.log('ID of fort clicked is ' + this.id);
+    // handle all bases that are not S1
+    if(this.id != "S1"){
+        console.log('inside not S1 conditional');
+        if(color == "default"){
+            this.setAttribute("data-color","red");
+            this.src = "./red/" + this.id + ".png";
+        }
+        else if(color == "red"){
+            this.setAttribute("data-color","blue");
+            this.src = "./blue/" + this.id + ".png";
+        }
+        else{
+            this.setAttribute("data-color","default");
+            this.src = "./default/" + this.id + ".png";
+        }
     }
     else{
-        this.setAttribute("data-color","default");
-        this.src = "./default/" + this.id + ".png";
+        if(isPugMode){
+            console.log("is pug running");
+            if(color == "default"){
+                this.setAttribute("data-color","red");
+                this.src = "./red/pug/S1_Pug.png";
+            }
+            else if(color == "red"){
+                this.setAttribute("data-color","blue");
+                this.src = "./blue/pug/S1_Pug.png";
+            }
+            else{
+                this.setAttribute("data-color","default");
+                this.src = "./default/pug/S1_Pug.png";
+            }
+        }
+        else{
+            if(color == "default"){
+                this.setAttribute("data-color","red");
+                this.src = "./red/" + this.id + ".png";
+            }
+            else if(color == "red"){
+                this.setAttribute("data-color","blue");
+                this.src = "./blue/" + this.id + ".png";
+            }
+            else{
+                this.setAttribute("data-color","default");
+                this.src = "./default/" + this.id + ".png";
+            }
+        }
     }
-
-    console.log(this.src);
 }
 
 
 for(let i = 0; i < forts.length;i++){
-    forts[i].addEventListener('click',toggleFortColor);
+    forts[i].addEventListener('click',toggleFortColor,false);
 }
+
+
+
+function editModeHandler(){
+    const edit_btn = document.getElementById("edit_mode");
+    const S1 = document.getElementById("S1");
+
+    // let isPugMode = true;
+    edit_btn.addEventListener('click',(e)=>{
+        // console.log('edit button being clicked');
+        // console.log('Is pug mode initial value ' + isPugMode);
+        // console.log(S1.src);
+        let color = S1.getAttribute('data-color');
+        console.log('color of button ' + color);
+        const normal_mode_btn = "./edit_modes/normal_mode_btn.png";
+        const pug_mode_btn = "./edit_modes/pug_mode_btn.png";
+        if(isPugMode){
+            S1.src = "./" + color + "/" + S1.id + ".png";
+            edit_btn.src =  normal_mode_btn;
+        }
+        else{
+            S1.src = "./" + color + "/pug/" + "S1_Pug.png";
+            edit_btn.src = pug_mode_btn;
+        }
+        isPugMode = !isPugMode;        
+        console.log('color of button ' + color);
+    });
+}
+
+function desktopCrownHandler(){
+    for(let i = 0; i < forts.length;i++){
+        forts[i].addEventListener('contextmenu',(event)=>{
+            event.preventDefault(); 
+            const id = event.target.id;
+            if(id == "S1" || id == "BaseBL" || id == "BaseTL" || id == "BaseBR" || id == "BaseTR")
+                return;
+            // if crown exist delete crown
+            const isCrownExist = document.getElementById(id + "_crown");
+            if(isCrownExist)
+                isCrownExist.remove();
+            else{
+                // Get Top And Left Property Values Of Javascript Element
+                const top = parseInt(window.getComputedStyle(event.target)["top"]);
+                const left = parseInt(window.getComputedStyle(event.target)["left"]);
+                // Create Img tag for crown and position on Top Of Fort
+                const img = document.createElement('img');
+                img.src = "crown_small.png";
+                img.style.zIndex = 2;
+                img.style.top = (top - 25) + "px";
+                img.style.left = (left + 11) + "px";
+                img.style.zIndex = 51;
+                img.id = id + "_crown";
+                img.classList.add("fort");
+                map.appendChild(img);
+            }
+        });
+    }
+}
+
+function cancel() {
+    clearTimeout(timer);
+  }
+
+function mobileCrownHandler(){
+    let timer;
+    for(let i = 0; i < i < forts.length;i++){
+        forts[i].addEventListener('touchstart',()=>{
+            timer = setTimeout(() => {
+                timer = null;
+                console.log('touched for 2 seconds');
+                // do stuff
+                
+            }, 2000);
+        });
+
+        forts[i].addEventListener('touchend',cancel);
+        forts[i].addEventListener('touchmove',cancel);
+    }
+}
+
+// function onLongPress(element, callback) {
+//     let timer;
+  
+//     element.addEventListener('touchstart', () => { 
+//       timer = setTimeout(() => {
+//         timer = null;
+//         callback();
+//       }, 500);
+//     });
+  
+
+  
+//     element.addEventListener('touchend', cancel);
+//     element.addEventListener('touchmove', cancel);
+//   }
+
+editModeHandler();
+desktopCrownHandler();
+
+
+
+
+    
+    
+
+
+
+// console.log(edit_btn.src);
 
 
 // Define the function 
